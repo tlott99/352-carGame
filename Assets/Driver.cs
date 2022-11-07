c1b4ccc6dd4b8881919eda662afa19947ab2a9d6
tlott99 <travis.lott@svu.edu>
2022-11-07 13:39:17 -0500

    capsule movement

---
+++ Assets/Driver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField]    float steerSpeed = .05f;
+    [SerializeField]    float moveSpeed = .0005f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(0,0,steerSpeed);
+        transform.Translate(0,moveSpeed,0);
+    }
+}
